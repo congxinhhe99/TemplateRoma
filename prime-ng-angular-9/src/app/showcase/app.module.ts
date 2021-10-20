535ee5eb76149dee29598b0d304ad318ed17b625
congxinhhe99 <conganhiikk9@gmail.com>
2021-10-20 20:48:38 +0700

    fix

---
+++ prime-ng-angular-9/src/app/showcase/app.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule }    from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AutoCompleteModule } from '../components/autocomplete/autocomplete';
+
+import { CarService } from './service/carservice';
+import { CountryService } from './service/countryservice';
+import { EventService } from './service/eventservice';
+import { NodeService } from './service/nodeservice';
+import { IconService } from './service/iconservice';
+import { CustomerService } from './service/customerservice';
+import { PhotoService } from './service/photoservice';
+import { VersionService } from './service/versionservice';
+import { AppConfigComponent } from './app.config.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AppConfigComponent
+  ],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    AutoCompleteModule
+  ],
+  providers: [
+      { provide: LocationStrategy, useClass: HashLocationStrategy },
+      CarService,CountryService,EventService,NodeService,IconService,CustomerService,PhotoService,VersionService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
